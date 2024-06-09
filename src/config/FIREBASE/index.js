da09e6b3ab99ce96ae99b043f1aa529e24e477b4
fernandatsaqif <f.tsaqif2004@gmail.com>
2024-06-09 22:07:06 +0700

    part 7

---
+++ src/config/FIREBASE/index.js
@@ -0,0 +1,14 @@
+import firebase from 'firebase'
+
+firebase.initializeApp({
+    apiKey: "AIzaSyBBFH15YOO9ge7pLhXSiwcBpvmlA4PEGJc",
+    authDomain: "crud-react-native-b9f39.firebaseapp.com",
+    projectId: "crud-react-native-b9f39",
+    storageBucket: "crud-react-native-b9f39.appspot.com",
+    messagingSenderId: "466384862676",
+    appId: "1:466384862676:web:aa50ed21e75881e5ab2a41"
+})
+
+const FIREBASE = firebase;
+
+export default FIREBASE;
---
+++ src/pages/TambahKontak/index.js
@@ -0,0 +1,98 @@
+import {Text, StyleSheet, View, TouchableOpacity, Alert} from 'react-native';
+import React, {Component} from 'react';
+import {InputData} from '../../components';
+import FIREBASE from '../../config/FIREBASE';
+
+export default class TambahKontak extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      nama: '',
+      nomorHP: '',
+      alamat: '',
+    };
+  }
+
+  onChangeText = (namaState, value) => {
+    this.setState({
+      [namaState]: value,
+    });
+  };
+
+  onSubmit = () => {
+    if (this.state.nama && this.state.nomorHP && this.state.alamat) {
+      const kontakRefrensi = FIREBASE.database().ref('Kontak');
+      const kontak = {
+        nama: this.state.nama,
+        nomorHP: this.state.nomorHP,
+        alamat: this.state.alamat,
+      };
+
+      kontakRefrensi
+        .push(kontak)
+        .then(data => {
+          Alert.alert('Sukses', 'Sukses kontak tersimpan');
+          this.props.navigation.replace('Home');
+        })
+        .catch(error => {
+          console.log('Error :', error);
+        });
+    } else {
+      Alert.alert('Peringatan', 'Nama, Nomor HP, dan Alamat wajib diisi!');
+    }
+  };
+
+  render() {
+    return (
+      <View style={styles.pages}>
+        <InputData
+          label="Nama"
+          placeholder="Masukkan Nama"
+          onChangeText={this.onChangeText}
+          value={this.state.nama}
+          namaState="nama"
+        />
+        <InputData
+          label="No. HP"
+          placeholder="Masukkan No. HP"
+          keyboardType="number-pad"
+          onChangeText={this.onChangeText}
+          value={this.state.nomorHP}
+          namaState="nomorHP"
+        />
+        <InputData
+          label="Alamat"
+          placeholder="Masukkan Alamat"
+          isTextArea={true}
+          onChangeText={this.onChangeText}
+          value={this.state.alamat}
+          namaState="alamat"
+        />
+
+        <TouchableOpacity style={styles.tombol} onPress={() => this.onSubmit()}>
+          <Text style={styles.textTombol}>SUBMIT</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  pages: {
+    flex: 1,
+    padding: 30,
+  },
+  tombol: {
+    backgroundColor: 'black',
+    padding: 10,
+    borderRadius: 5,
+    marginTop: 10,
+  },
+  textTombol: {
+    color: 'white',
+    fontWeight: 'bold',
+    textAlign: 'center',
+    fontSize: 16,
+  },
+});
